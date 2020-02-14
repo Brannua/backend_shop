c78ab138863a045bf493ae21410e5257461e217a
Brannua <374453156@qq.com>
2020-02-14 21:40:35 +0800

    code review

--- controller/cart.js
+++ controller/cart.js
@@ -2,24 +2,6 @@ const Router = require('koa-router'),
   mongoose = require('mongoose');
 let router = new Router();
 
-router.post('/addProductToCart', async (ctx) => {
-  const Cart = mongoose.model('Cart'),
-    cart = new Cart(ctx.request.body);
-  await cart.save()
-    .then((res) => {
-      ctx.body = {
-        code: 200,
-        message: '添加成功',
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      ctx.body = {
-        code: 500,
-        message: '添加购物车失败',
-      }
-    });
-});
 router.get('/getCartByUserId', async (ctx) => {
   const Cart = mongoose.model('Cart');
   await Cart.find({ userId: ctx.query.userId }).populate('productId').exec()
@@ -37,6 +19,25 @@ router.get('/getCartByUserId', async (ctx) => {
     }
   });
 });
+
+router.post('/addProductToCart', async (ctx) => {
+  const Cart = mongoose.model('Cart'),
+    cart = new Cart(ctx.request.body);
+  await cart.save()
+    .then(() => {
+      ctx.body = {
+        code: 200,
+        message: '添加成功',
+      }
+    })
+    .catch(() => {
+      ctx.body = {
+        code: 500,
+        message: '添加购物车失败',
+      }
+    });
+});
+
 router.post('/deleteProductInCartByProductId', async (ctx) => {
   const Cart = mongoose.model('Cart');
   await Cart.deleteOne({ productId: ctx.request.body.productId }).exec()
